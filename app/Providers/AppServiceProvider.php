75a0497b379cc7159f73b8e8bb7da7eebe296a4b
Emanuel Martinez <emanuelmartinezw@hotmail.com>
2022-07-28 19:45:49 -0300

    fix: force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\ServiceProvider;
 
@@ -25,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
